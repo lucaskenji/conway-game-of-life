78660dbf5d5ec94c21bc9a1e48ec1a1953fdd5d6
Lucas <ra1957066@gmail.com>
2020-12-17 13:31:56 -0300

    Refactored code

--- gameoflife.js
+++ gameoflife.js
@@ -1,180 +1,148 @@
-$(document).ready(function() {
-	// Draw grid
-	let rows = Array(20);
-	let cols = Array(400);
-	for (let i = 0; i < 20; i++) {
-		rows[i] = "<tr id=\"row" + String(i) + "\"></tr>";
-		for (let j = 0; j < 20; j++) {
-			cols[j + (i * 20)] = "<td class=\"cell\" id=\"cell" + String(j + (i*20)) + "\">&nbsp;</td>";
-		}
-	}
-	$("#grade").html(rows.join(""));
-	for (let i = 0; i < 20; i++) {
-		$("#row" + String(i)).html(cols.slice(i * 20, i * 20 + 20));			
-	}
-	$(".cell").css("background-color","darkgray");
-	// Create grid as array
-	// "grade" is a bidimensional array, with 20 rows each with 20 columns
-	// "pregrade" is an auxiliar array, a copy of "grade", because it's necessary during each generation.
-	let grade = Array(20);
-	let pregrade = Array(20);
-	for (let i = 0; i < 20; i++) {
-		grade[i] = Array(20);
-		pregrade[i] = Array(20);
-		for (let j = 0; j < 20; j++) {
-			grade[i][j] = false;
-			pregrade[i][j] = false;
-		}
-	}
-	// Basic setup, 1 cell on the middle
-	$("#cell209").css("background-color", "green");
-	grade[10][9] = true;
-	let gen_number = 0;
-	let cell_number = 1;
-	
-	function howManyNeighbours(x, y) {
-		let count = 0;
-		// Check top neighbours
-		if (x > 0) {
-			if (y > 0) {
-				if (pregrade[x-1][y-1] == true) {
-					count++;
-				}
-			}
-			if (y < 19) {
-				if (pregrade[x-1][y+1] == true) {
-					count++;
-				}
-			}
-			if (pregrade[x-1][y] == true) {
-				count++;
-			}
-		}
-		// Check neighbours on the same level
-		if (y > 0) {
-			if (pregrade[x][y-1] == true) {
-				count++;
-			}
-		}
-		if (y < 19) {
-			if (pregrade[x][y+1] == true) {
-				count++;
-			}
-		}
-		// Check neighbours on the bottom level
-		if (x < 19) {
-			if (y > 0) {
-				if (pregrade[x+1][y-1] == true) {
-					count++;
-				}
-			}
-			if (y < 19) {
-				if (pregrade[x+1][y+1] == true) {
-					count++;
-				}
-			}
-			if (pregrade[x+1][y] == true) {
-				count++;
-			}
-		}
-		return count;
-	};
-	
-	function changeState(x, y) {
-		// If the cell is dead but there's 3 neighbours, reproduction insues
-		let state = pregrade[x][y];
-		let neighbours = howManyNeighbours(x, y);
-		if (state == false && neighbours == 3) {
-			grade[x][y] = true;
-			cell_number++;
-			return;
-		}
-		// If a cell is alive
-		if (state) {
-			if (neighbours < 2) {
-				// less than 2 alive neighbours, dies from starvation(?)
-				cell_number--;
-				grade[x][y] = false;
-			} else if (neighbours > 3) {
-				// more than 3 alive neighbours, dies from overpopulation
-				cell_number--;
-				grade[x][y] = false;
-			}
-			// With 2 or 3 neighbours, remains alive.
-			return;
-		}
-	}
-	
-	function updateGrade(repeat) {
-		for (let k = 0; k < repeat; k++) {
-			gen_number++;
-			for (let i = 0; i < 20; i++) {
-				for (let j = 0; j < 20; j++) {
-					pregrade[i][j] = grade[i][j];
-				}
-			}
-			for (let i = 0; i < 20; i++) {
-				for (let j = 0; j < 20; j++) {
-					changeState(i, j);
-					if (grade[i][j] == false) {
-						$("#cell" + String((i * 20) + j)).css("background-color", "darkgray");
-					} else {
-						$("#cell" + String((i * 20) + j)).css("background-color", "green");
-					}
-				}
-			}
-		}
-	}
-	
-	function updateValues() {
-		$("#gen_number").text(String(gen_number));
-		$("#cell_number").text(String(cell_number));
-	}
-	
-	$("#botao").click(function() {
-		updateGrade(1);
-		updateValues();
-	});
-	
-	$("#botao10").click(function() {
-		updateGrade(10);
-		updateValues();
-	});
-	
-	$("#botaoclear").click(function() {
-		for (let i = 0; i < 20; i++) {
-			for (let j = 0; j < 20; j++) {
-				grade[i][j] = false;
-				pregrade[i][j] = false;
-				$("#cell" + String((i * 20) + j)).css("background-color", "darkgray");
-			}
-		}
-		cell_number = 0;
-		gen_number = 0;
-		updateValues();
-	});
-	
-	$(".cell").click(function() {
-		let id_clicked = Number($(this).attr("id").replace("cell", ""));
-		// Gets row and column position of clicked cell
-		let pos_r = 0;
-		let pos_c = 0;
-		if (((id_clicked - (id_clicked % 10))/10) % 2 == 0) {
-			pos_c = id_clicked % 10;
-			pos_r = (id_clicked - (id_clicked % 10)) / 20;
-		} else {
-			pos_c = id_clicked % 10 + 10;
-			pos_r = ((id_clicked - (id_clicked % 10)) / 20) - 0.5;
-		}
-		// Kills or gives birth to cell in said position
-		grade[pos_r][pos_c] = !grade[pos_r][pos_c];
-		if (grade[pos_r][pos_c] == true) {
-			$(this).css("background-color", "green");
-			cell_number++;
-		} else {
-			$(this).css("background-color", "darkgray");
-			cell_number--;
-		}
-		updateValues();
-	});
-});
+function drawGrid(CELL_SIZE, CELL_BORDER, table) {
+    // Draws an grid of cells of CELL_SIZE, separated by CELL_BORDER.
+    // The grid contains an equal amount of rows and columns of table.length
+    const canvas = document.getElementById("canvas");
+    const canvasContext = canvas.getContext("2d");
+    canvas.width = (CELL_SIZE + CELL_BORDER) * table.length;
+    canvas.height = (CELL_SIZE + CELL_BORDER) * table.length;
+
+    for (let col = 0; col < table.length; col++) {
+        for (let row = 0; row < table.length; row++) {
+            canvasContext.fillStyle = table[row][col] ? "green" : "lightgray";
+            canvasContext.fillRect((col * (CELL_SIZE + CELL_BORDER)), (row * (CELL_SIZE + CELL_BORDER)), CELL_SIZE, CELL_SIZE);
+        }
+    }  
+}
+
+function updateGenNumber(offset) {
+    const genSpan = document.getElementById("gen_number");
+    const currentGen = parseInt(genSpan.innerHTML, 10);
+    genSpan.innerHTML = currentGen + offset;
+}
+
+function updateCellNumber(offset) {
+    const cellSpan = document.getElementById("cell_number");
+    const currentCells = parseInt(cellSpan.innerHTML, 10);
+    cellSpan.innerHTML = currentCells + offset;
+}
+
+function getTable(size) {
+    // Returns a table(in this case an array of arrays) of size "size"
+    const table = [];
+    for (let i = 0; i < size; i++) {
+    table[i] = Array(size);
+    }
+
+    return table;
+}
+
+function changeCellState(CELL_SIZE, CELL_BORDER) {
+    return (x, y, table) => {
+        // Enables or disables cell on coordinates (y, x)
+        const cellCol = Math.floor(x / (CELL_SIZE + CELL_BORDER));
+        const cellRow = Math.floor(y / (CELL_SIZE + CELL_BORDER));
+
+        table[cellRow][cellCol] = !Boolean(table[cellRow][cellCol]);
+        
+        // Update cell count text
+        updateCellNumber(table[cellRow][cellCol] ? 1 : -1);
+    }
+}
+
+function changeGeneration(table) {
+    // Table needs to be copied because the original one is modified during analysis.
+    const tableCopy = [];
+    
+    for (let i = 0; i < table.length; i++) {
+        tableCopy.push(table[i].slice());
+    }
+    
+    const getCountNeighbors = (row, col) => {
+        // rowLevels and colLevels contain, respectively, the possible adjacent rows and columns.
+        // adjacentCells stores all values from the possible adjacent cells, true or false.
+        // Finally, the return value is the amount of "true" values, equivalent to one neighbor for each "true".
+        const rowLevels = [row - 1, row, row + 1].filter((val) => val >= 0 && val < tableCopy.length);
+        const colLevels = [col - 1, col, col + 1].filter((val) => val >= 0 && val < tableCopy.length);
+        const adjacentCells = [];
+        
+        for (let i = 0; i < rowLevels.length; i++) {
+            for (let j = 0; j < colLevels.length; j++) {
+                adjacentCells.push(tableCopy[rowLevels[i]][colLevels[j]]);
+            }
+        }
+        
+        const result = adjacentCells.reduce((count = 0, hasNeighbor) => count += hasNeighbor ? 1 : 0);
+        
+        // Removing one from the count result if it's alive, otherwise it will count itself as a neighbor
+        return tableCopy[row][col] ? result - 1 : result;
+    }
+    
+    let neighbors = 0;
+    
+    for (let row = 0; row < table.length; row++) {
+        for (let col = 0; col < table.length; col++) {
+            neighbors = getCountNeighbors(row, col);
+
+            // If the current cell is dead, reproduction may occur if there are exactly 3 neighbors
+            if (!table[row][col]) {
+                if (neighbors == 3) {
+                    table[row][col] = true;
+                    updateCellNumber(1); // adds one to cell count text
+                }
+                continue;
+            }
+            
+            // If the current cell is alive, it will die unless it has exactly 2 or 3 neighbors
+            if (neighbors < 2 || neighbors > 3) {
+                table[row][col] = false;
+                updateCellNumber(-1); // removes one from cell count text
+            }
+        }
+    }
+    
+    // Change generation text
+    updateGenNumber(1);
+}
+
+window.onload = () => {
+    const CELL_SIZE = 30;
+    const CELL_BORDER = 2;
+    const TABLE_SIZE = 12;
+    
+    let table = getTable(TABLE_SIZE);
+    
+    const canvas = document.getElementById("canvas");
+    const nextGenButton = document.getElementById("next-btn");
+    const skipGenButton = document.getElementById("skip-btn");
+    const clearButton = document.getElementById("clear-btn");
+    
+    canvas.addEventListener("mousedown", (e) => {
+        // offsetX and offsetY return the position of the mouse click relative to the canvas
+        changeCellState(CELL_SIZE, CELL_BORDER)(e.offsetX, e.offsetY, table);
+        drawGrid(CELL_SIZE, CELL_BORDER, table);
+    })
+    
+    nextGenButton.addEventListener("click", () => {
+        changeGeneration(table);
+        drawGrid(CELL_SIZE, CELL_BORDER, table);
+    })
+    
+    skipGenButton.addEventListener("click", () => {
+        for (let i = 0; i < 10; i++) {
+            changeGeneration(table);
+        }
+        drawGrid(CELL_SIZE, CELL_BORDER, table);
+    })
+    
+    clearButton.addEventListener("click", () => {
+        table = table.map((row) => row.map(() => false));
+        drawGrid(CELL_SIZE, CELL_BORDER, table);
+        
+        // Also reset cell and gen count
+        const cellSpan = document.getElementById("cell_number");
+        const genSpan = document.getElementById("gen_number");
+        cellSpan.innerHTML = 0;
+        genSpan.innerHTML = 0;
+    })
+    drawGrid(CELL_SIZE, CELL_BORDER, table);
+}
